29edd8ac5c452a73a5be5c579ef78dc18338b7dc
Daniel Chapman <scriptor.deo@gmail.com>
2020-10-02 15:30:44 -0400

    first working version

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
 const Parser = require("rss-parser");
-const http = require("http");
+const axios = require("axios").default;
+const fileDownload = require("js-file-download");
 const fs = require("fs");
 const prompt = require("prompt-sync")({ sigint: true });
+const slugify = require("slugify");
+const async = require("async");
+
+function download(url, filename) {}
+
 let rssParser = new Parser();
-fs.mkdir("./output", (error) => {
-  if (error) {
-    return console.error(error);
-  }
-});
+if (!fs.existsSync("./output")) {
+  fs.mkdir("./output", (error) => {
+    if (error) {
+      return console.error(error);
+    }
+  });
+} else {
+  console.log("Output folder already exists.");
+}
+
 const rssUrl = prompt("Enter the url of the RSS feed you want to download: ");
 console.log(`You entered ${rssUrl}`);
 (async () => {
   const feed = await rssParser.parseURL(rssUrl);
   const { items } = feed;
-  items.map((item) => {
+
+  for (const item of items) {
     const { title, pubDate } = item;
-    const url = item.enclosure.url;
-    console.log({ title }, { pubDate }, { url });
-  });
+    const date = new Date(pubDate);
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const url = item.enclosure.url.replace(/\?.*$/g, "");
+    const filename = `./output/${year}-${month}-${day}-${slugify(title, {
+      lower: true,
+      strict: true,
+    })}.mp3`;
+
+    await axios
+      .get(url, {
+        responseType: "stream",
+        timeout: 100000,
+      })
+      .then((res) => {
+        res.data.pipe(fs.createWriteStream(filename));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log(`"${title}" downloaded!`);
+  }
 })();
