1c7548d0f70db0b11796cfe2714bdfb8fdb0c608
Daniel Chapman <scriptor.deo@gmail.com>
2020-10-03 15:38:20 -0400

    migrate to a next app

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Form from "../src/form";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>This is my Website</h1>
+
+      <Form />
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/form.js
@@ -0,0 +1,10 @@
+function Form() {
+  return (
+    <form>
+      <input type="url" placeholder="Enter the url for an RSS feed"></input>
+      <input type="submit" />
+    </form>
+  );
+}
+
+export default Form;
